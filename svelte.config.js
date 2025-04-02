1cc4b19add280cf1aee32d2c92e74b8d1956c1bd
CodingFreeze <codingfreeze@example.com>
2025-04-02 07:13:31 -0400

    Switch from GitHub Pages to Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: { 
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/AlgoVault' : ''
-		}
+		adapter: adapter(),
 	},
 	extensions: ['.svelte', '.svx']
 };
