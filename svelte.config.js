7e737484b12c17a19ad189208b8f080d448a1788
CodingFreeze <codingfreeze@example.com>
2025-04-02 06:48:44 -0400

    Setup GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,22 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { 
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/AlgoVault' : ''
+		}
+	},
 	extensions: ['.svelte', '.svx']
 };
 
